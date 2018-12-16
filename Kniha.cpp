0ac70a76789553e56055b2d64e53574afb2386c0
Zdeněk Pašek <zdenekpasek@Zdenek-MacBook-Air.local>
2018-12-16 18:46:52 +0100

    Initial commit

---
+++ Kniha.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zdeněk Pašek on 13/12/2018.
+//
+
+#include "Kniha.h"
---
+++ Kniha.h
@@ -0,0 +1,14 @@
+//
+// Created by Zdeněk Pašek on 13/12/2018.
+//
+
+#ifndef SEMESTRALKA_KNIHA_H
+#define SEMESTRALKA_KNIHA_H
+
+
+class Kniha {
+    int pocet = 20;
+};
+
+
+#endif //SEMESTRALKA_KNIHA_H
